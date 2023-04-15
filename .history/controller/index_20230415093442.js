d6f398e198b573d8cf34ecc7af40079eecc577fe
DESKTOP-CKVKEEH\Khoi Nguyen <phamkhoinguyen.12b2@gmail.com>
2023-04-15 10:42:28 +0700

    add-product

---
+++ .history/controller/index_20230415093442.js
@@ -0,0 +1,49 @@
+getProductForCarousel = () => {
+  let getProductForCarousel = axios({
+    url: "https://shop.cyberlearn.vn/api/Product",
+    method: "GET",
+  })
+    .then((resp) => {
+      let listShoes = resp.data.content;
+      console.log(resp.data.content);
+      for (let i = 0; i < 5; i++) {
+        let output = "";
+        let shoe = listShoes[i];
+        output = `
+          <div class="container">
+            <div class="product_img">
+              <img src="${shoe.image}" alt="" />
+            </div>
+            <div class="product_detail">
+              <h2>${shoe.name}</h2>
+              <p>${shoe.description}</p>
+              <button>Buy Now</button>
+            </div>
+          </div>
+          `;
+        document.querySelector(`.owl-carousel .item${i}`).innerHTML = output;
+        console.log(document.querySelector(`.owl-carousel .item${i}`));
+      }
+    })
+    .catch((err) => {
+      document.querySelector(
+        ".owl-carousel"
+      ).innerHTML = `<h3 class="text-center mt-5">Oops ! Somethings Went Wrong <br> Please come back later !</h3>`;
+    });
+};
+
+getProductForCarousel();
+
+getProduct = () => {
+  let getProduct = axios({
+    url: 'https://shop.cyberlearn.vn/api/Product/getProductByCategory?categoryId=adidas',
+    method: "GET"
+  }).then (
+    resp => {
+      let listAdidas = resp.data.content;
+    }
+  ).catch (err => {
+  })
+}
+
+getProduct();
---
+++ .history/controller/index_20230415100056.js
@@ -0,0 +1,105 @@
+getProductForCarousel = () => {
+  let getProductForCarousel = axios({
+    url: "https://shop.cyberlearn.vn/api/Product",
+    method: "GET",
+  })
+    .then((resp) => {
+      let listShoes = resp.data.content;
+      console.log(resp.data.content);
+      for (let i = 0; i < 5; i++) {
+        let output = "";
+        let shoe = listShoes[i];
+        output = `
+          <div class="container">
+            <div class="product_img">
+              <img src="${shoe.image}" alt="" />
+            </div>
+            <div class="product_detail">
+              <h2>${shoe.name}</h2>
+              <p>${shoe.description}</p>
+              <button>Buy Now</button>
+            </div>
+          </div>
+          `;
+        document.querySelector(`.owl-carousel .item${i}`).innerHTML = output;
+        console.log(document.querySelector(`.owl-carousel .item${i}`));
+      }
+    })
+    .catch((err) => {
+      document.querySelector(
+        ".owl-carousel"
+      ).innerHTML = `<h3 class="text-center mt-5">Oops ! Somethings Went Wrong <br> Please come back later !</h3>`;
+    });
+};
+
+getProductForCarousel();
+
+getProduct = () => {
+  let getProduct = axios({
+    url: "https://shop.cyberlearn.vn/api/Product/getProductByCategory?categoryId=adidas",
+    method: "GET",
+  })
+    .then((resp) => {
+      let listAdidas = resp.data.content;
+      console.log(listAdidas);
+      let output = "";
+      listAdidas.forEach((shoe) => {
+        output += `            <div class="col-12 col-sm-6 col-md-4">
+        <div class="container">
+          <div class="card">
+            <img src="${shoe.image}" class="card-img-top" alt="..." />
+            <div class="card-body">
+              <h5 class="card-title">${shoe.name}</h5>
+              <p class="card-text">
+                ${shoe.shortDescription}
+              </p>
+              <button class="more-info">
+              <a href="#">More Info</a>
+              </button>
+
+            </div>
+          </div>
+        </div>
+      </div>`;
+        document.querySelector(".adidas-row").innerHTML = output;
+      });
+    })
+    .catch((err) => {});
+};
+
+getProduct();
+
+getAdidas = () => {
+  let getProduct = axios({
+    url: "https://shop.cyberlearn.vn/api/Product/getProductByCategory?categoryId=adidas",
+    method: "GET",
+  })
+    .then((resp) => {
+      let listAdidas = resp.data.content;
+      console.log(listAdidas);
+      let output = "";
+      listAdidas.forEach((shoe) => {
+        output += `            <div class="col-12 col-sm-6 col-md-4">
+        <div class="container">
+          <div class="card">
+            <img src="${shoe.image}" class="card-img-top" alt="..." />
+            <div class="card-body">
+              <h5 class="card-title">${shoe.name}</h5>
+              <p class="card-text">
+                ${shoe.shortDescription}
+              </p>
+              <button class="more-info">
+              <a href="#">More Info</a>
+              </button>
+
+            </div>
+          </div>
+        </div>
+      </div>`;
+        document.querySelector(`.adidas-swiper .item${i}`).innerHTML = output;
+      });
+    })
+    .catch((err) => {});
+};
+
+getAdidas();
