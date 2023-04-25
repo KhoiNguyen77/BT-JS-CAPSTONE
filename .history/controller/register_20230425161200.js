caf02f86c64cfde71beaf18cee10febbd171dadf
DESKTOP-CKVKEEH\Khoi Nguyen <phamkhoinguyen.12b2@gmail.com>
2023-04-25 16:13:27 +0700

    done

--- .history/controller/register_20230425161200.js
+++ .history/controller/register_20230425161200.js
@@ -39,7 +39,7 @@ document.querySelector('#registerSub').onclick = function () {
         data: usersNew
     }).then(res => {
         console.log(res);
-        window.alert(res.data.message)
+        window.alert(res.response.data.message)
     }).catch(err => {
         window.alert(err.response.data.message)
     })
