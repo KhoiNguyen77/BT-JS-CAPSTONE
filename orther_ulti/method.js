14d65be86979b5f79bdd6878ba2a992447b2da00
thanh trinh <trinhphuthanh95@gmail.com>
2023-04-25 18:37:27 +0700

    thanh commit 2

--- orther_ulti/method.js
+++ orther_ulti/method.js
@@ -14,10 +14,10 @@ function checkEmail(value, name) {
 
     let regexEmail = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
     if (regexEmail.test(value)) {
-        document.querySelector(`#error-regex-${name}`).innerHTML = '';
+        document.querySelector(`#error-required-${name}`).innerHTML = '';
         return true;
     }
-    document.querySelector(`#error-regex-${name}`).innerHTML = `Please enter correct Email !`;
+    document.querySelector(`#error-required-${name}`).innerHTML = `Please enter correct ${name} !`;
     return false;
 
 
@@ -28,11 +28,11 @@ function checkNumber(value, name) {
     console.log(value);
     let regexNumber = /^[0-9]+$/;
     if (regexNumber.test(value)) {
-        document.querySelector(`#error-regex-${name}`).innerHTML = '';
+        document.querySelector(`#error-required-${name}`).innerHTML = '';
         return true;
     }
     else {
-        document.querySelector(`#error-regex-${name}`).innerHTML = `Please enter correct ${name} number !`;
+        document.querySelector(`#error-required-${name}`).innerHTML = `Please enter correct ${name} number !`;
         return false;
     }
 
@@ -41,22 +41,49 @@ function checkNumber(value, name) {
 function checkText(value, name) {
     let regexLetter = /^[A-Za-z\s]+$/;;
     if (regexLetter.test(value)) {
-        document.querySelector(`#error-regex-${name}`).innerHTML = '';
+        document.querySelector(`#error-required-${name}`).innerHTML = '';
         return true;
     }
-    document.querySelector(`#error-regex-${name}`).innerHTML = `Please enter correct ${name} !`;
+    document.querySelector(`#error-required-${name}`).innerHTML = `Please enter correct ${name} !`;
     return false;
 }
 
 function checkPassword(value, name) {
-    let regexPass = /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,10}$/;
+    let regexPass = /^[A-Za-z]\w{6,10}$/;
     if (regexPass.test(value)) {
-        document.querySelector(`#error-regex-${name}`).innerHTML = '';
+        document.querySelector(`#error-required-${name}`).innerHTML = '';
         return true;
     }
-    document.querySelector(`#error-regex-${name}`).innerHTML = `${name} need  !`;
+    document.querySelector(`#error-required-${name}`).innerHTML = `
+    ${name} need 6 to 10 characters,one uppercase and one lowercase letter!`;
     return false;
 }
+function confirmPass(pass, name) {
+
+    let confPass = document.querySelector("#passConfirm").value
+    if (pass == confPass) {
+        //alert('Wrong confirm password !');
+        document.querySelector(`#error-${name}`).innerHTML = '';
+        return true;
+    }
+
+    document.querySelector(`#error-${name}`).innerHTML = `Wrong confirm ${name} `;
+    return false;
+
+}
+// function checkGender(name) {
+//     let genderM = document.querySelector('#male');
+//     let genderF = document.querySelector('#female');
+//     if (!genderM.checked && !genderF.checked) {
+
+//         document.querySelector(`#error-${name}`).innerHTML = 'You must select male or female';
+//     }
+//     else {
+//         document.querySelector(`#error-${name}`).innerHTML = '';
+//     }
+// }
+
+
 
 function checkValid(obj) {
     let valid = true;
@@ -65,9 +92,15 @@ function checkValid(obj) {
         checkEmpty(obj.password, 'Password') &
         checkEmpty(obj.name, 'Name') &
         checkEmpty(obj.phone, 'Phone');
-
-    valid += valid & checkEmail(obj.email, 'Email');
-    valid += valid & checkNumber(parseInt(obj.phone), 'Phone');
-    valid += valid & checkText(obj.name, 'Name');
+    //check email
+    valid = valid & checkEmail(obj.email, 'Email');
+    //check Number
+    valid = valid & checkNumber(parseInt(obj.phone), 'Phone')
+    //check Text
+    valid = valid & checkText(obj.name, 'Name');
+    //check password
+    valid = valid & checkPassword(obj.password, 'Password') & confirmPass(obj.password, 'Password');
+    //check gender
+    // valid = valid & checkGender('Gender');
     return valid;
 }
